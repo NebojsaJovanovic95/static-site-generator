e282e2e763bc7d1a88105d8066b9f2c7dd3a0af0
Nebojsa Jovanovic <nebojsajovanovic95@gmail.com>
2026-01-16 18:29:59 -0500

    CH2L4

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,18 +1,3 @@
-class Tag:
-    def __init__(self, tag):
-        self.tag = tag
-    
-    def to_html(self, close = False, **kwargs) -> str:
-        if self.tag:
-            tag += "<"
-            if close:
-                tag += "/"
-            tag += self.tag
-        for key, value in kwargs.item():
-            tag += f"{key}={value}"
-        if self.tag:
-            tag += ">"
-        return tag
 
 
 class HtmlNode:
@@ -21,7 +6,7 @@ class HtmlNode:
         tag: Tag = None,
         value: str = None,
         children: str = None,
-        props: str = None,
+        props: dict = None,
     ):
         self.tag = tag
         self.value = value
@@ -36,7 +21,33 @@ class HtmlNode:
 
     def to_html(self) -> str:
         raise NotImplementedError("method not implemented for this html node")
-        return f"{self.tag.to_html(self.props)}\n{self.value}\n{self.children}\n{self.tag.to_html(close = True)}"
 
     def __repr__(self):
-        return f"HtmlNode(tag={self.tag}, value={self.value}, children=[{self.children}], props=[{self.__props_to_html()])"
+        return f"HtmlNode(tag={self.tag}, value={self.value}, children=[{self.children}], props=[{self.__props_to_html()}])"
+
+
+class LeafNode(HtmlNode):
+    def __init__(
+        self,
+        tag: str,
+        value: str,
+        props: dict = None
+    ):
+        super().__init__(
+            tag = tag,
+            value = value,
+            props = props
+        )
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("value is None")
+        if tag is None:
+            return self.value
+        return f"<{self.tag}{self.__props_to_html()}>{self.value}</{self.tag}>"
+
+    def __repr__(self):
+        return f"HtmlNode(tag={self.tag}, value={self.value}, props=[{self.__props_to_html()}])"
+
+
+
